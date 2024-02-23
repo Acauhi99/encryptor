2722af30dc8d81bc5358db8099f3034e465ae24c
Acauhi <acauhi.mateus@gmail.com>
2024-02-23 11:55:36 -0300

    update encrypt function

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const SHIFT = 3;
-const ENCRYPTED_ALPHABET = ALPHABET.slice(SHIFT) + ALPHABET.slice(0, SHIFT);
-
+const KEY = 3;
 const buttonEncrypt = document.getElementsByClassName('content__button__encrypt')[0];
-const buttonDecrypt = document.getElementsByClassName('content__button__decrypt');
+const buttonDecrypt = document.getElementsByClassName('content__button__decrypt')[0];
 const textareaMessage = document.getElementsByClassName('text__input')[0];
 
-
-function encryptMessage(message) {
+function encryptMessage(message, key) {
   let encryptedMessage = "";
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
   for (let i = 0; i < message.length; i++) {
-    const charIndex = ALPHABET.indexOf(message[i]);
-    if (charIndex !== -1) {
-      encryptedMessage += ENCRYPTED_ALPHABET[charIndex];
+    const element = message[i].toLowerCase();
+    if (alphabet.includes(element)) {
+      const shiftedChar = shiftCharacter(element, key, alphabet);
+      encryptedMessage += shiftedChar;
     } else {
-      encryptedMessage += message[i];
+      encryptedMessage += element;
     }
   }
   return encryptedMessage;
 }
 
+function shiftCharacter(char, shiftValue, alphabet) {
+  if (!alphabet.includes(char)) {
+    return char;
+  }
+  const shiftedIndex = (alphabet.indexOf(char) + shiftValue) % alphabet.length;
+  return alphabet[shiftedIndex];
+}
+
 buttonEncrypt.addEventListener('click', () => {
   const message = textareaMessage.value;
-  const encryptedMessage = encryptMessage(message);
+  const encryptedMessage = encryptMessage(message, KEY);
 
   const outputTextContainer = document.getElementById('outputText');
   const outputMessage = outputTextContainer.querySelector('.content__text__output__p');
